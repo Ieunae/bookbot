86e00c5838b2ed32811c063930d7bee4eda6c92a
Ieunae <ieuanjones192001@hotmail.com>
2025-10-09 15:53:57 +0100

    refactor to stats.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def word_count(text):
-    words = text.split()
-    return len(words)
+from stats import word_count
 
 main()
